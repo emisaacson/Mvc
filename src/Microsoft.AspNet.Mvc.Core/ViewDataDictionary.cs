a9110e882a9c8cb78b4e41794e3f891500bcc8c0
Elliot Isaacson <elliot@emikek.com>
2015-05-11 07:01:03 -0400

    Use more logical naming.

--- src/Microsoft.AspNet.Mvc.Core/ViewDataDictionary.cs
+++ src/Microsoft.AspNet.Mvc.Core/ViewDataDictionary.cs
@@ -186,8 +186,8 @@ namespace Microsoft.AspNet.Mvc
         {
             // This is the core constructor called when Model is known.
             var modelType = GetModelType(model);
-            var modelExplorerModelType = Nullable.GetUnderlyingType(source.ModelMetadata.ModelType) ?? source.ModelMetadata.ModelType;
-            if (modelType == modelExplorerModelType && model == source.ModelExplorer.Model)
+            var modelMetadataModelType = Nullable.GetUnderlyingType(source.ModelMetadata.ModelType) ?? source.ModelMetadata.ModelType;
+            if (modelType == modelMetadataModelType && model == source.ModelExplorer.Model)
             {
                 // Preserve any customizations made to source.ModelExplorer.ModelMetadata if the Type
                 // that will be calculated in SetModel() and source.Model match new instance's values.
@@ -353,8 +353,8 @@ namespace Microsoft.AspNet.Mvc
             // null. When called from the Model setter, ModelMetadata will (temporarily) be null. When called from
             // a constructor, current ModelMetadata may already be set to preserve customizations made in parent scope.
             var modelType = GetModelType(value);
-            var modelExplorerModelType = Nullable.GetUnderlyingType(ModelExplorer?.Metadata.ModelType) ?? ModelExplorer?.Metadata.ModelType;
-            if (modelExplorerModelType != modelType)
+            var modelMetadataModelType = Nullable.GetUnderlyingType(ModelExplorer?.Metadata.ModelType) ?? ModelExplorer?.Metadata.ModelType;
+            if (modelMetadataModelType != modelType)
             {
                 ModelExplorer = _metadataProvider.GetModelExplorerForType(modelType, value);
             }
